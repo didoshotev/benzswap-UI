3eb588199309580acecd73e22c1566e2fd32aa37
didoshotev <didoshotev@gmail.com>
2022-07-06 00:02:16 +0300

    helper jsons

--- pages/lottery.tsx
+++ pages/lottery.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from 'next'
+import { useWeb3Contract } from 'react-moralis'
 
 const Lottery: NextPage = () => {
     
+    // const { runContractFunction } = useWeb3Contract({ 
+    //     abi: ,
+    //     contract: ,
+    //     functionName:  
+    //     params: { },
+    //     msgValue: 
+    // })    
+
     return (
         <>
             <h2>Lottery Page</h2>
